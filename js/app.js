c110e303d70099c70877169658c9d957bffd9cc3
shuvo9620 <smahfuzur0@gmail.com>
2023-03-02 23:28:39 +0600

    Dynamic Card Added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,26 @@ const loadAiUniverse = async() =>{
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.tools);
+    displayAiUniverse(data.data.tools);
+}
+
+const displayAiUniverse = aiUniverses =>{
+    const dataContainer = document.getElementById('data-container');
+    aiUniverses.forEach(aiUniverse =>{
+        const dataDiv = document.createElement('div')
+        dataDiv.classList.add('col');
+        dataDiv.innerHTML = `
+        <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.</p>
+        </div>
+     </div>
+        `;
+        dataContainer.appendChild(dataDiv);
+    })
 }
 
 loadAiUniverse();
