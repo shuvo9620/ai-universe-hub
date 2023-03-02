c18020e7e718c596f4c265ee75bcafe8aeae35b7
shuvo9620 <smahfuzur0@gmail.com>
2023-03-03 00:08:53 +0600

    display 6 cards only and button added

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ const loadAiUniverse = async() =>{
 
 const displayAiUniverse = aiUniverses =>{
     const dataContainer = document.getElementById('data-container');
+    // display 6 cards only
+    aiUniverses = aiUniverses.slice(0,6);
     aiUniverses.forEach(aiUniverse =>{
         const dataDiv = document.createElement('div')
         dataDiv.classList.add('col');
         dataDiv.innerHTML = `
-        <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <div class="card p-4 h-100">
+        <img src="${aiUniverse.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${aiUniverse.published_in}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                 to additional content. This content is a little bit longer.</p>
         </div>
