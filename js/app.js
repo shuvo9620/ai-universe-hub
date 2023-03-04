38e7f469765845c1a4030127145ecdcc62cdeffd
shuvo9620 <smahfuzur0@gmail.com>
2023-03-04 10:48:22 +0600

    features dynamic value added

--- js/app.js
+++ js/app.js
@@ -9,21 +9,26 @@ const displayAiUniverse = aiUniverses =>{
     const dataContainer = document.getElementById('data-container');
     // display 6 cards only
     aiUniverses = aiUniverses.slice(0,6);
+    // display all data
     aiUniverses.forEach(aiUniverse =>{
+        const {features, id, image, name, published_in} = aiUniverse;
         const dataDiv = document.createElement('div')
         dataDiv.classList.add('col');
         dataDiv.innerHTML = `
         <div class="card p-4 h-100">
-        <img src="${aiUniverse.image}" class="card-img-top" alt="...">
+        <img src="${image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${aiUniverse.published_in}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">Features</h5>
+            <p class="card-text">${features ? features.map((item_list, index) => `<br> <span>${index + 1 }.${item_list}</span>`) : "Feature not found"}</p>
         </div>
      </div>
         `;
         dataContainer.appendChild(dataDiv);
     })
 }
+//  see more
+// document.getElementById('btn-see-more').addEventListener('click', function(){
+    
+// })
 
 loadAiUniverse();
